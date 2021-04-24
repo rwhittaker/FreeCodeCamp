5a3df8698d934396b1e1c4bf3a95a86641d8ef98
rwhittaker <ryanpeterwhittaker@gmail.com>
2021-04-24 14:12:19 +0100

    steamroll

---
+++ Intermediate Algorithms/steamrollArray.js
@@ -0,0 +1,15 @@
+function steamrollArray(arr) {
+
+    var flat = []; 
+  
+    for (var i = 0; i < arr.length; i++){
+      if(Array.isArray(arr[i])){
+        flat.push(...steamrollArray(arr[i]))
+      } else {
+        flat.push(arr[i])
+      }
+    } 
+    return flat;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
