08dddfe5c9b334b07c669645095b128e7211071d
rwhittaker <ryanpeterwhittaker@gmail.com>
2021-04-26 13:21:11 +0100

    numerals

--- JS Algorithms and Data Structure Projects/RomanNumerals.js
+++ JS Algorithms and Data Structure Projects/RomanNumerals.js
@@ -1,19 +1,20 @@
 function convertToRoman(num) {
-    arr = [];
-    var lookup = {M:1000, CM:900, DCCC:800, DCC:700, DC:600, D:500,CD:400,CCC:300,CC:200,C:100,XC:90,LXXX:80,LXX:70,LX:60,L:50,XL:40,XXX:30,XX:20,XI:11,X:10,IX:9,VIII:8,VII:7,VI:6,V:5,IV:4,III:3,II:2,I:1};
-    
-    console.log(Object.keys(lookup));
-    console.log(Object.entries(lookup));
-
-
-
-
-    //console.log(Math.floor(2001/1000))
-    
-    return arr.join('');
-
+    var lookup = {M:1000, CM:900, DCCC:800, DCC:700, DC:600, D:500,CD:400,CCC:300,CC:200,C:100,XC:90,LXXX:80,LXX:70,LX:60,L:50,XL:40,XXX:30,XX:20,X:10,IX:9,VIII:8,VII:7,VI:6,V:5,IV:4,III:3,II:2,I:1};
+    var roman ='';
+    var i = 0;
+    // I is key
+    for(i in lookup){
+        while(num >= lookup[i]){
+            // console.log("I is: "+i);
+            // Push the current key to the roman string
+            roman += i;
+            // Subtract the current key's value from num.
+            // console.log("num is: "+num);
+            num -= lookup[i]
+        }
     }
-    
-    console.log(convertToRoman(2000));
-    console.log(convertToRoman(500));
-    console.log(convertToRoman(100));
+    return roman;
+    }
+console.log(convertToRoman(16));
+console.log(convertToRoman(2014));
+
